978f6887ca5087c4ab66f6148eb0471f8989569a
Bruna Wundervald <brunadaviesw@gmail.com>
2018-05-10 22:34:26 -0300

    :D

---
+++ R/craio.R
@@ -0,0 +1,26 @@
+#' craio
+#'
+#' A Caio's random last name generator. 
+#' @param l numeric. The parameter to generate a random index
+#'
+#' @return The generated full name. 
+#' @examples{
+#' \donttest{
+#'   craio(5)
+#'}
+#'}
+#' @export
+
+craio <- function(l){
+  d <- lexiconPT::oplexicon_v2.1
+  pal <- stringr::str_detect(d$term, "ente$")
+  names <- d[pal, ]$term
+  ind <- rpois(n = 1, lambda = l)*3 
+  
+  if(ind <= length(names)){
+    
+    full_name <- stringr::str_c("Caio ", stringr::str_to_title(names[ind]))
+    print(full_name)
+  } 
+}
+
---
+++ README.md
@@ -0,0 +1,21 @@
+Caio's random last name generator. 
+
+``` r
+# install.packages("devtools")
+devtools::install_github("datathonbr/craio")
+purrr::map(5:15, craio)
+
+[1] "Caio Ardente"
+[1] "Caio Adormecente"
+[1] "Caio Cadente"
+[1] "Caio Comprazente"
+[1] "Caio Autosuficiente"
+[1] "Caio Benevolente"
+[1] "Caio Benevolente"
+[1] "Caio Consequente"
+[1] "Caio Comprazente"
+[1] "Caio Comovente"
+[1] "Caio Comprazente"
+```
+
+
